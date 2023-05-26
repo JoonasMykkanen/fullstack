81d053c8d99fd18ce89d83fffaacd5f5b8c64a0b
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-05-26 23:57:26 +0300

    adding ex2.11

--- part_2/puhelinluettelo/src/App.js
+++ part_2/puhelinluettelo/src/App.js
@@ -1,13 +1,10 @@
 import { List, PersonForm, FilterForm } from './components/Phonebook.js'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456' },
-		{ name: 'Ada Lovelace', number: '39-44-5323523' },
-		{ name: 'Dan Abramov', number: '12-43-234345' },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122' }
-	  ])	
+	const [persons, setPersons] = useState([])
   	const [newName, setNewName] = useState('')
   	const [number, setNumber] = useState('')
   	const [filter, setFilter] = useState('')
@@ -15,6 +12,16 @@ const App = () => {
   	const nameChange = (event) => setNewName(event.target.value)
   	const numberChange = (event) => setNumber(event.target.value)
   	const updateFilter = (event) => setFilter(event.target.value)
+
+	useEffect(() => {
+		console.log('effect')
+		axios
+		.get('http://localhost:3001/persons')
+		.then(response => {
+			console.log('promise fulfilled')
+			setPersons(response.data)
+		})
+	}, [])
 
 	const handleClick = (event) => {
 		event.preventDefault()
