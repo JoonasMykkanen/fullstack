6b60a640e33af38243c7e23b9d553b526bc55106
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-09-25 09:37:25 +0300

    ex 4.4

--- part_4/blog_backend/tests/utils.test.js
+++ part_4/blog_backend/tests/utils.test.js
@@ -1,8 +1,43 @@
 const listHelper = require('../utils/list_helper')
 
+const blogsSample = [
+	{
+		title: 'First Blog',
+		author: 'John Doe',
+		url: 'http://example.com/first-blog',
+		likes: 5
+	},
+	{
+		title: 'Second Blog',
+		author: 'Jane Smith',
+		url: 'http://example.com/second-blog',
+		likes: 15
+	},
+	{
+		title: 'Third Blog',
+		author: 'Sam Adams',
+		url: 'http://example.com/third-blog',
+		likes: 10
+	}
+]
+
 test('dummy returns one', () => {
 	const blogs = []
 
 	const result = listHelper.dummy(blogs)
 	expect(result).toBe(1)
+})
+
+describe('totalikes', () => {
+	test('total likes calculate correct sum for empty array', () => {
+		const blogs = []
+
+		const result = listHelper.totalLikes(blogs)
+		expect(result).toBe(0)
+	})
+
+	test('total likes with three arrays', () => {
+		const result = listHelper.totalLikes(blogsSample)
+		expect(result).toBe(30)
+	})
 })
--- part_4/blog_backend/utils/list_helper.js
+++ part_4/blog_backend/utils/list_helper.js
@@ -3,6 +3,14 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	const sum = blogs.reduce(function(sum, blog) {
+		return sum + blog.likes
+	}, 0)
+	return sum
+}
+
 module.exports = {
+	totalLikes,
 	dummy
 }
