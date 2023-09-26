ed30fd612a58bb9dc31d341f614cd7e2d1ac9c64
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-09-26 06:35:17 +0300

    pushing ex 4.5 - 4.7

--- part_4/blog_backend/tests/utils.test.js
+++ part_4/blog_backend/tests/utils.test.js
@@ -1,5 +1,26 @@
 const listHelper = require('../utils/list_helper')
 
+const blogs = []
+
+const listOfOne = [
+	{
+		title: 'only one',
+		author: 'Joonas',
+		url: 'http//joonas.com/blog',
+		likes: 100
+	}
+]
+
+const retMostBlogsOne = {
+	author: 'Joonas',
+	blogs: 1
+}
+
+const retMostLikesOne = {
+	author: 'Joonas',
+	likes: 100
+}
+
 const blogsSample = [
 	{
 		title: 'First Blog',
@@ -11,33 +32,103 @@ const blogsSample = [
 		title: 'Second Blog',
 		author: 'Jane Smith',
 		url: 'http://example.com/second-blog',
-		likes: 15
+		likes: 10
 	},
 	{
 		title: 'Third Blog',
 		author: 'Sam Adams',
 		url: 'http://example.com/third-blog',
 		likes: 10
+	},
+	{
+		title: 'Fourth Blog',
+		author: 'Sam Adams',
+		url: 'http://example.com/Fourth-blog',
+		likes: 7
 	}
 ]
 
-test('dummy returns one', () => {
-	const blogs = []
+const retMostBlogsSample = {
+	author: 'Sam Adams',
+	blogs: 2
+}
 
+const retMostLikesSample = {
+	author: 'Sam Adams',
+	likes: 17
+}
+
+
+
+test('dummy returns one', () => {
 	const result = listHelper.dummy(blogs)
 	expect(result).toBe(1)
 })
 
+
+
 describe('totalikes', () => {
 	test('total likes calculate correct sum for empty array', () => {
-		const blogs = []
-
 		const result = listHelper.totalLikes(blogs)
 		expect(result).toBe(0)
 	})
 
+	test('list of one', () => {
+		const result = listHelper.totalLikes(listOfOne)
+		expect(result).toBe(100)
+	})
+
 	test('total likes with three arrays', () => {
 		const result = listHelper.totalLikes(blogsSample)
-		expect(result).toBe(30)
+		expect(result).toBe(32)
+	})
+})
+
+
+
+describe('favoriteblog', () => {
+	test('empty list', () => {
+		const result = listHelper.favoriteBlog(blogs)
+		expect(result).toBe(0)
+	})
+	test('list of one', () => {
+		const result = listHelper.favoriteBlog(listOfOne)
+		expect(result).toEqual(listOfOne[0])
+	})
+	test('Longer list', () => {
+		const result = listHelper.favoriteBlog(blogsSample)
+		expect(result).toEqual(blogsSample[1])
+	})
+})
+
+
+
+describe('most blogs by author', () => {
+	test('empty list', () => {
+		const result = listHelper.mostBlogs(blogs)
+		expect(result).toBe(0)
+	})
+	test('list of one', () => {
+		const result = listHelper.mostBlogs(listOfOne)
+		expect(result).toEqual(retMostBlogsOne)
+	})
+	test('Longer list', () => {
+		const result = listHelper.mostBlogs(blogsSample)
+		expect(result).toEqual(retMostBlogsSample)
+	})
+})
+
+describe('most likes by author', () => {
+	test('empty list', () => {
+		const result = listHelper.mostLikes(blogs)
+		expect(result).toBe(0)
+	})
+	test('list of one', () => {
+		const result = listHelper.mostLikes(listOfOne)
+		expect(result).toEqual(retMostLikesOne)
+	})
+	test('Longer list', () => {
+		const result = listHelper.mostLikes(blogsSample)
+		expect(result).toEqual(retMostLikesSample)
 	})
 })
--- part_4/blog_backend/utils/list_helper.js
+++ part_4/blog_backend/utils/list_helper.js
@@ -1,5 +1,6 @@
-const dummy = (blogs) => {
-	console.log(blogs)
+const lodash = require('lodash')
+
+const dummy = () => {
 	return 1
 }
 
@@ -10,7 +11,50 @@ const totalLikes = (blogs) => {
 	return sum
 }
 
+const favoriteBlog = (blogs) => {
+	if (blogs.length === 0)
+		return 0
+
+	let current = blogs.reduce(function(current, blog) {
+		if (blog.likes > current.likes)
+			return blog
+		else
+			return current
+	}, blogs[0])
+
+	return current
+}
+
+const mostBlogs = (blogs) => {
+	if (blogs.length === 0)
+		return 0
+
+	const groupedByAuthor = lodash.groupBy(blogs, 'author')
+	const transformedOutput = lodash.map(groupedByAuthor, item => ({ author: item[0].author, blogs: item.length }))
+	const sortedOutput = lodash.orderBy(transformedOutput, ['blogs'], ['desc'])
+	const result = lodash.head(sortedOutput)
+
+	return result
+}
+
+const mostLikes = (blogs) => {
+	if (blogs.length === 0)
+		return 0
+
+	const groupedByAuthor = lodash.groupBy(blogs, 'author')
+	const transformedOutput = lodash.map(groupedByAuthor, item => ({
+		author: item[0].author, likes: lodash.sumBy(item, 'likes')
+	}))
+	const sortedOutput = lodash.orderBy(transformedOutput, ['likes'], ['desc'])
+	const result = lodash.head(sortedOutput)
+
+	return result
+}
+
 module.exports = {
+	favoriteBlog,
 	totalLikes,
+	mostLikes,
+	mostBlogs,
 	dummy
 }
